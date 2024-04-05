8f3b6b9aa1a9d12cc27391bf83972af0ad64d1db
LalatenduBhatta <lalatendubhatta@gmail.com>
2024-04-05 09:21:20 +0530

    js basics

---
+++ JS/jsNotes.js
@@ -0,0 +1,46 @@
+// JAVASCRIPT
+
+// JAVASCRIPT IS A HIGH LEVEL PROGRAMMING LANGUAGE ,MOSTLY USED FOR
+// CREATING CLIENT SIDE WEBSITE OR APPLICATION DEVELOPEMENT
+
+// JS IS A JUST IN TIME (JIT) COMPILATION ,INTERPRETED PROGRAMMING
+// LANGUAGE
+
+// JS IS SYNCHRONOUS AND SINGLE THREADED
+
+// JRE
+
+// HISTORY
+// BRENDAN EICH
+
+// NETSCAPE NAVIGATOR EMPLOYED CLIENT SIDE RENDERING LANGUAGE
+
+// NETSCAPE 2 BROWSER BETA VERSION
+
+// MOCHA LIVESCRIPT
+
+// 1997 ECMA-SCRIPT
+
+
+// var a = 10
+// let b = "xyz"
+// const c = true
+
+// function print() {
+//     let name = "virat"
+//     console.log(name)//virat
+// }
+// print()
+// console.log(name);//reference error
+
+
+// let number = 10
+// let number2 = 20
+
+// console.log(number, number2)
+
+// function show() {
+//     console.log(number)
+// }
+
+// show()
